d3521ecf0dde90ec0423cc0b00b04e35bb16feab
Tom Schulz <tomschulz@Toms-MBP.attlocal.net>
2023-05-24 16:02:33 -0700

    Restart again

---
+++ src/app/api/apiHelper.ts
@@ -0,0 +1,17 @@
+export const BEARER_TOKEN = process.env.TWITTER_API_KEY as string;
+
+export async function fetchWithToken(url: string, options: RequestInit = {}): Promise<Response> {
+    const defaultHeaders: Record<string, string> = {
+        'Authorization': `Bearer ${BEARER_TOKEN}`,
+    };
+
+    options.headers = options.headers ? {...options.headers as Record<string, string>, ...defaultHeaders} : defaultHeaders;
+
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+
+    return response;
+}
---
+++ types.d.ts
@@ -0,0 +1,5 @@
+type User = {
+    id: number,
+    name: string,
+    username: string,
+  }
