46dacd08bac1ed023d1dad26bb97b5a95612c286
Tom Schulz <tomschulz@Toms-MBP.attlocal.net>
2023-05-24 16:33:07 -0700

    Inter is back

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
-// import { Inter } from 'next/font/google';
+import { Inter } from 'next/font/google';
 
-// const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Create Next App',
@@ -27,7 +27,7 @@ export default function RootLayout({
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
       </head>
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
