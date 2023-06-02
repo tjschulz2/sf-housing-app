5210a4655ed465da0ea9edbd8d98d2a4a5523ee5
neallseth <32910906+neallseth@users.noreply.github.com>
2023-06-01 20:46:00 -0400

    adding styles to invite modal

--- components/invite-button/invite-button.tsx
+++ components/invite-button/invite-button.tsx
@@ -1,6 +1,18 @@
 "use client";
 import { useState } from "react";
 import Modal from "../modal/modal";
+import styles from "./invite-button.module.css";
+
+const referralLink = "http://yourReferralLink.com";
+
+const copyToClipboard = async () => {
+  try {
+    await navigator.clipboard.writeText(referralLink);
+    alert("Referral link copied to clipboard!");
+  } catch (err) {
+    console.error("Failed to copy text: ", err);
+  }
+};
 
 export default function InviteButton() {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,10 +27,35 @@ export default function InviteButton() {
 
   return (
     <>
-      <button onClick={openModal}>Open Modal</button>
+      <a className={styles.inviteButton} onClick={openModal}>
+        Invite a friend
+      </a>
       <Modal closeModal={closeModal} isOpen={isOpen}>
-        <p>Modal text content</p>
-        <button onClick={closeModal}>Close</button>
+        <div
+          style={{
+            position: "relative",
+            marginBottom: "16px",
+            marginLeft: "16px",
+            marginRight: "16px",
+          }}
+        >
+          <button
+            onClick={closeModal}
+            style={{ position: "absolute", top: 0, right: 0 }}
+          >
+            x
+          </button>
+          <h2>Invite a friend</h2>
+          <p style={{ color: "grey" }}>
+            Only refer people you know, trust, or think would be a good fit for
+            this directory. Referring randoms will get your referral link
+            reversed.
+          </p>
+          <div className={styles.referralClipboard} onClick={copyToClipboard}>
+            <img style={{ marginRight: "8px" }} src="./link.svg" />
+            {referralLink}
+          </div>
+        </div>
       </Modal>
     </>
   );
