6d306b617a891d05814d1cbe98639a69ae9735ad
Tom Schulz <tomschulz@Toms-MBP.attlocal.net>
2023-06-21 13:56:33 -0700

    Fixing error codes again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,15 +14,17 @@ import { error } from "console";
 const Home: NextPage = () => {
   const router = useRouter();
   const referralCode = useSearchParams().get("referralCode");
-  const errorCode = useSearchParams().get("error");
+  const errorDescription = useSearchParams().get("error_description");
   const [referralDetails, setReferralDetails] = useState<ReferralDetails>(null);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function handlePageLoad() {
       // Check for error query parameter in URL
-      if (errorCode) {
+      if (errorDescription === "Error getting user email from external provider") {
         alert('You need to add your email address to your Twitter account. \n\nGo to Twitter -> More -> Settings and Support -> Your account -> Email. \n\nAfter you do this, try again.');
+      } else if (errorDescription) {
+        alert('You got an error:\n\n' + errorDescription + '\n\nContact @thomasschulzz on Twitter to investigate.');
       }
       if (referralCode) {
         const referral = await getReferralDetails(referralCode);
