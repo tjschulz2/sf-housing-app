4d18cccaf6d772d3431c57aa38c4aa330ec3b958
neallseth <32910906+neallseth@users.noreply.github.com>
2023-12-09 15:41:28 -0500

    updating referral badge, attempting form zoom fix

--- src/components/referral-badge.tsx
+++ src/components/referral-badge.tsx
@@ -1,27 +1,43 @@
+"use client";
 import UserProfileImage from "@/components/user-profile-image";
+import { getReferrerName } from "@/lib/utils/data";
+import { useEffect, useState } from "react";
+import LoadingSpinner from "./loading-spinner/loading-spinner";
 
-export default function ReferralBadge({
-  handle,
-  imageURL,
-  name,
-}: {
-  handle: string;
-  imageURL: string;
-  name: string;
-}) {
-  if (!handle || !imageURL || !name) {
-    return;
+type ReferrerData = {
+  name: string | null;
+  twitter_handle: string | null;
+  twitter_avatar_url: string | null;
+};
+
+export default function ReferralBadge({ userID }: { userID: string }) {
+  const [referrer, setReferrer] = useState<ReferrerData | null>(null);
+  useEffect(() => {
+    const fetchReferrer = async () => {
+      try {
+        const referrerData = await getReferrerName(userID);
+        if (referrerData) setReferrer(referrerData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchReferrer();
+  }, [userID]);
+
+  if (referrer) {
+    return (
+      <a
+        href={`https://x.com/${referrer.twitter_handle}`}
+        className="flex items-center no-underline"
+      >
+        <span className="mx-1">
+          <UserProfileImage size="small" src={referrer.twitter_avatar_url} />
+        </span>
+        <span className="text-blue-500 hover:text-blue-400">
+          {referrer.name}
+        </span>
+      </a>
+    );
   }
-
-  return (
-    <a
-      href={`https://x.com/${handle}`}
-      className="flex items-center no-underline"
-    >
-      <span className="mx-1">
-        <UserProfileImage size="small" src={imageURL} />
-      </span>
-      <span className="text-blue-500 hover:text-blue-400">{name}</span>
-    </a>
-  );
 }
