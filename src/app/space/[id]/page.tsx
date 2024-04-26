9e96a9a29f24aebafb38b0a4a4965b9be5fba7c0
neallseth <32910906+neallseth@users.noreply.github.com>
2024-04-25 23:02:16 -0700

    deleting old space dir

--- src/app/space/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getSpaceDetails } from "@/lib/utils/data";
-
-export default async function Page({ params }: { params: { id: string } }) {
-  const spaceDetails = await getSpaceDetails(Number(params.id));
-  if (!spaceDetails) {
-    return <p>{spaceDetails}</p>;
-  }
-  return <div>{JSON.stringify(spaceDetails)}</div>;
-}
