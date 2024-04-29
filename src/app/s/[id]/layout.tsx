84e8a2c12a4777ac05bd825a706fc6a4c9a6cec7
neallseth <32910906+neallseth@users.noreply.github.com>
2024-04-28 19:24:38 -0700

    reordering page elements, adding email button

--- src/app/s/[id]/layout.tsx
+++ src/app/s/[id]/layout.tsx
@@ -4,7 +4,7 @@ export default function SpacePageLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="w-full flex justify-center">
+    <div className="w-full flex justify-center bg-neutral-100 min-h-dvh">
       <div className="max-w-md ">{children}</div>
     </div>
   );
