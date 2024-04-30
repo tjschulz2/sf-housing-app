5116d60ee3b078c2c9ef0e98d0d342b0fdb98d7e
neallseth <32910906+neallseth@users.noreply.github.com>
2024-04-30 00:12:04 -0700

    updating layout, upgrading tailwind

--- src/app/s/[id]/layout.tsx
+++ src/app/s/[id]/layout.tsx
@@ -4,8 +4,8 @@ export default function SpacePageLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="w-full flex justify-center bg-neutral-100 min-h-dvh">
-      <div className="max-w-md ">{children}</div>
+    <div className="w-full flex justify-center items-center lg:justify-normal bg-neutral-100 min-h-dvh">
+      <div className="max-w-md lg:max-w-full">{children}</div>
     </div>
   );
 }
--- src/app/s/[id]/page.tsx
+++ src/app/s/[id]/page.tsx
@@ -19,77 +19,82 @@ export default async function Page({ params }: { params: { id: string } }) {
     return <p>404</p>;
   }
   return (
-    <div className="p-6 flex flex-col gap-8">
+    <div className="p-6 flex flex-col lg:flex-row gap-8">
       {spaceDetails.image_url ? (
-        <img className="rounded-xl w-full" src={spaceDetails.image_url} />
+        <img
+          className="rounded-xl w-full lg:max-w-md h-fit"
+          src={spaceDetails.image_url}
+        />
       ) : null}
-      <h1 className="text-2xl md:text-3xl font-bold tracking-wide text-center">
-        {spaceDetails.name}
-      </h1>
+      <div className="flex flex-col gap-8">
+        <h1 className="text-2xl md:text-3xl font-bold tracking-wide text-center">
+          {spaceDetails.name}
+        </h1>
 
-      <div className="flex gap-4">
-        {spaceDetails.location ? (
+        <div className="flex justify-evenly">
+          {spaceDetails.location ? (
+            <div className="flex gap-1 items-center">
+              <img src="/location.svg" />
+              <p id="location">{housingMap.location[spaceDetails.location]}</p>
+            </div>
+          ) : null}
+
           <div className="flex gap-1 items-center">
-            <img src="/location.svg" />
-            <p id="location">{housingMap.location[spaceDetails.location]}</p>
+            <img src="/threepeople.svg" />
+            <p>{spaceDetails.resident_count}</p>
           </div>
-        ) : null}
 
-        <div className="flex gap-1 items-center">
-          <img src="/threepeople.svg" />
-          <p>{spaceDetails.resident_count}</p>
-        </div>
-
-        {/* {spaceDetails.room_price_range ? (
+          {/* {spaceDetails.room_price_range ? (
             <p>
               {housingMap.roomPrice[spaceDetails.room_price_range]}
             </p>
           ) : null} */}
-      </div>
+        </div>
 
-      <div className="bg-neutral-200 p-4 rounded-xl flex flex-col gap-2">
-        <p>{spaceDetails.description}</p>
-        {spaceDetails.website_url ? (
-          <div className="flex items-center">
+        <div className="bg-neutral-200 p-4 rounded-xl flex flex-col gap-2 whitespace-pre-line">
+          <p>{spaceDetails.description}</p>
+          {spaceDetails.website_url ? (
             <a className="text-blue-400" href={spaceDetails.website_url}>
               {spaceDetails.website_url}
             </a>
-            {/* <ExternalLink className="h-4 w-4 ml-1 text-blue-400" /> */}
-          </div>
-        ) : null}
-      </div>
+          ) : null}
+        </div>
 
-      {/* <ContactMeButton
+        {/* <ContactMeButton
           phoneNum={spaceDetails.contact_phone}
           email={spaceDetails.contact_email}
           twitter={userDetails.twitter_handle}
         /> */}
-      <div className="flex" id="host-section">
-        <img
-          className="w-32 h-32 rounded-xl"
-          src={userDetails?.twitter_avatar_url || ""}
-        />
-        <div className="flex flex-col justify-center gap-2 items-center grow">
-          <p className="text-xl font-semibold">{userDetails?.name}</p>
-          <div>
-            <Link
-              href={`https://x.com/${userDetails?.twitter_handle}`}
-              className="flex items-center justify-center w-full"
-              target="_blank"
-            >
-              <span className="text-blue-500 hover:text-blue-400 max-w-full truncate">
-                @{userDetails?.twitter_handle}
-              </span>
-              <TwitterLogo className="ml-1 overflow-visible" fill="#3191e7" />
-            </Link>
+        <div className="flex" id="host-section">
+          <img
+            className="w-32 h-32 rounded-xl"
+            src={userDetails?.twitter_avatar_url || ""}
+          />
+          <div className="flex flex-col justify-center gap-2 items-center grow">
+            <p className="text-xl font-semibold">{userDetails?.name}</p>
+            <div>
+              <Link
+                href={`https://x.com/${userDetails?.twitter_handle}`}
+                className="flex items-center justify-center w-full"
+                target="_blank"
+              >
+                <span className="text-blue-500 hover:text-blue-400 max-w-full truncate">
+                  @{userDetails?.twitter_handle}
+                </span>
+                <TwitterLogo className="ml-1 overflow-visible" fill="#3191e7" />
+              </Link>
+            </div>
+            {spaceDetails.contact_email ? (
+              <Link
+                target="_blank"
+                href={`mailto:${spaceDetails.contact_email}`}
+              >
+                <Button variant="outline" size="icon">
+                  <Mail className="h-4 w-4" />
+                </Button>
+              </Link>
+            ) : null}{" "}
           </div>
-          {spaceDetails.contact_email ? (
-            <Link target="_blank" href={`mailto:${spaceDetails.contact_email}`}>
-              <Button variant="outline" size="icon">
-                <Mail className="h-4 w-4" />
-              </Button>
-            </Link>
-          ) : null}{" "}
         </div>
       </div>
     </div>
