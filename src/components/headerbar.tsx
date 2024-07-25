fc61aecac7d7b204fc18f2db69501e5676d11084
Neall Seth <32910906+neallseth@users.noreply.github.com>
2024-07-24 18:35:13 -0700

    commenting out 'list a rental' for unauth landing page (#83)

--- src/components/headerbar.tsx
+++ src/components/headerbar.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import styles from './home-page-component.module.css';
+import { useState } from "react";
+import styles from "./home-page-component.module.css";
 
 export default function HeaderBar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,15 +9,17 @@ export default function HeaderBar() {
   };
 
   return (
-    <header className="
+    <header
+      className="
       bg-[#FEFBEB]
       flex justify-between items-center
       px-4 sm:px-10 py-5
       border-b border-black border-opacity-10
       relative
-    ">
-      <a 
-        href="https://solarissf.com" 
+    "
+    >
+      <a
+        href="https://solarissf.com"
         className="
           flex flex-row items-center space-x-3 
           hover:bg-[#E7E8D9] hover:text-[#005B41] 
@@ -33,21 +35,32 @@ export default function HeaderBar() {
         <text className={styles.branding}>SOLARIS</text>
       </a>
       <div className="md:hidden">
-        <button 
-          onClick={toggleMenu} 
+        <button
+          onClick={toggleMenu}
           className="
             text-[#1D462F]
             hover:text-[#005B41]
             focus:outline-none
           "
         >
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
           </svg>
         </button>
       </div>
       <nav className="hidden md:flex md:items-center md:space-x-6">
-        <ul className="md:flex md:space-x-6">
+        {/* <ul className="md:flex md:space-x-6">
           {[
             { name: 'List a rental', link: 'https://airtable.com/appurOWXAegMj76UY/pagI9io5qFhw7F264/form' }
           ].map((item) => (
@@ -65,23 +78,31 @@ export default function HeaderBar() {
               </a>
             </li>
           ))}
-        </ul>
+        </ul> */}
       </nav>
       {isOpen && (
-        <div className="
+        <div
+          className="
           absolute top-full left-0 w-full bg-[#FEFBEB] border-t border-black border-opacity-10
           md:hidden z-50
-        ">
+        "
+        >
           <nav>
             <ul className="flex flex-col items-center space-y-4 py-4">
               {[
-                { name: 'Solaris AI', link: 'https://www.solarissf.com/solaris-ai/root' },
-                { name: 'DirectorySF', link: 'https://directorysf.com' },
-                { name: 'Email', link: 'mailto:thomas@solarissf.com?subject=Let%27s%20talk%20Solaris' },
-                { name: 'Twitter', link: 'https://x.com/solarissociety' }
+                {
+                  name: "Solaris AI",
+                  link: "https://www.solarissf.com/solaris-ai/root",
+                },
+                { name: "DirectorySF", link: "https://directorysf.com" },
+                {
+                  name: "Email",
+                  link: "mailto:thomas@solarissf.com?subject=Let%27s%20talk%20Solaris",
+                },
+                { name: "Twitter", link: "https://x.com/solarissociety" },
               ].map((item) => (
                 <li key={item.name}>
-                  <a 
+                  <a
                     href={item.link}
                     className="
                       font-noto-serif text-[#1D462F] font-semibold 
