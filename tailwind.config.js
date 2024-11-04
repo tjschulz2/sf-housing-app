da5cfa10879416b1869b1bc712e957df8113319a
neallseth <32910906+neallseth@users.noreply.github.com>
2024-11-04 00:21:43 -0800

    sponsorship page

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,6 +46,7 @@ module.exports = {
   },
   plugins: [
     require("tailwindcss-animate"),
+    require("@tailwindcss/typography"),
     addVariablesForColors,
     function ({ matchUtilities, theme }) {
       matchUtilities(
