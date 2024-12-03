2d0f2a7055f5daf1ecab6646c05a61e141a5e01e
Neall <32910906+neallseth@users.noreply.github.com>
2024-12-02 22:56:30 -0500

    add user hover card to referral badge

--- src/components/referral-badge.tsx
+++ src/components/referral-badge.tsx
@@ -1,9 +1,10 @@
 "use client";
 import UserProfileImage from "@/components/user-profile-image";
-import { getReferrerName } from "@/lib/utils/data";
+import { getReferrerData } from "@/lib/utils/data";
 import { useEffect, useState } from "react";
 import LoadingSpinner from "./loading-spinner/loading-spinner";
 import { Skeleton } from "@/components/ui/skeleton";
+import { UserHoverCard } from "@/components/cards/user-hover-card";
 
 type ReferrerData = {
   name: string | null;
@@ -20,12 +21,12 @@ export default function ReferralBadge({
   showAvatar?: boolean;
   textSize?: "xs" | "sm" | "md";
 }) {
-  const [referrer, setReferrer] = useState<ReferrerData | null>(null);
+  const [referrer, setReferrer] = useState<MemberUserType | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     const fetchReferrer = async () => {
       try {
-        const referrerData = await getReferrerName(userID);
+        const referrerData = await getReferrerData(userID);
         if (referrerData) setReferrer(referrerData);
         setIsLoading(false);
       } catch (error) {
@@ -38,20 +39,27 @@ export default function ReferralBadge({
 
   if (referrer) {
     return (
-      <a
-        href={`https://x.com/${referrer.twitter_handle}`}
-        className="flex items-center no-underline"
-        target="_blank"
-      >
-        {showAvatar ? (
-          <span className="mx-1">
-            <UserProfileImage size="small" src={referrer.twitter_avatar_url} />
+      <UserHoverCard userData={referrer}>
+        <a
+          href={`https://x.com/${referrer.twitter_handle}`}
+          className="flex items-center no-underline"
+          target="_blank"
+        >
+          {showAvatar ? (
+            <span className="mx-1">
+              <UserProfileImage
+                size="small"
+                src={referrer.twitter_avatar_url}
+              />
+            </span>
+          ) : null}
+          <span
+            className={`text-blue-500 hover:text-blue-400 text-${textSize}`}
+          >
+            {referrer.name?.split(" ").slice(0, 2).join(" ")}
           </span>
-        ) : null}
-        <span className={`text-blue-500 hover:text-blue-400 text-${textSize}`}>
-          {referrer.name?.split(" ").slice(0, 2).join(" ")}
-        </span>
-      </a>
+        </a>
+      </UserHoverCard>
     );
   } else if (isLoading) {
     return <Skeleton className="h-4 w-[75px]" />;
