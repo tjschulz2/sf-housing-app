8ae10ace62ba7314930e82ec82f61169b4f85738
Neall <32910906+neallseth@users.noreply.github.com>
2024-12-03 01:22:00 -0500

    init jobs

---
+++ src/components/cards/job-card.tsx
@@ -0,0 +1,22 @@
+import UserProfileImage from "@/components/user-profile-image";
+import ReferralBadge from "@/components/referral-badge";
+import { Card, CardTop, CardBottom } from "@/components/cards/card";
+import TwitterLogo from "@/images/twitter-logo.svg";
+import Link from "next/link";
+import { formatDateMonthYear } from "@/lib/utils/general";
+import { CalendarDays } from "lucide-react";
+
+export default function JobCard() {
+  return (
+    <Card className="shadow">
+      <div className="flex items-center justify-between gap-4">
+        <div className="flex flex-col gap-2 basis-1/3"></div>
+        <div className="flex flex-col gap-2 grow">
+          <div className="flex gap-2 text-sm">
+            <span className="text-xs text-gray-600">Referred by</span>
+          </div>
+        </div>
+      </div>
+    </Card>
+  );
+}
